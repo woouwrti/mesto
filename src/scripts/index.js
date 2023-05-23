2ccf9df4f5229d28005f95070d3697a41bb7a9b5
Kir <woouwrti@gmail.com>
2023-05-23 20:29:00 +0500

    eighth sprint webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css';
+
 import { initialCards } from './cards.js' // format { name, link }
 import { validationSettings } from './validate.js'
 import Card from './Card.js'
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true // сайт будет открываться сам при запуске npm run dev
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        'postcss-loader']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+}
